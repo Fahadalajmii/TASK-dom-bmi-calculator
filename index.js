6a481ae6be4d39ef9575f9320b76397157aacb1a
fahad alajmi <fahad@fahads-MacBook-Pro.local>
2022-01-06 19:56:15 +0300

    already graded

--- index.js
+++ index.js
@@ -7,5 +7,27 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
+  let bmi = weight / ((height/100) ^2) ; 
+  alert(bmi) ;
+
+  if (bmi <18.5) 
+  {alert("Under weight")}
+
+  else if (bmi <2.5 && bmi >= 18.5)
+  {
+    alert("healthy weight")
+  }
+  else if (bmi <30 && bmi >=25)
+  {
+    alert("over weight")
+  }
+  else if (bmi >= 30)
+  {
+    alert ("Above obesity")
+  }
+  
+
+
   // You are going to write the code here before the curly braces 👇🏻
 }
+
